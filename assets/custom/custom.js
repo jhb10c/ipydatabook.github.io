a11ec55bf1c9a0ae06aa3f8086145e5843170a23
ipydatabook <jhbergschneider@ung.edu>
2020-12-27 02:30:25 -0500

    Update custom.js

--- assets/custom/custom.js
+++ assets/custom/custom.js
@@ -1,5 +1,6 @@
 
 //edit
+/*
 window.onload = function() {
     var anchors = document.querySelectorAll('a[href*="mybinder.org"]');
     Array.prototype.forEach.call(anchors, function (element, index) {
@@ -35,4 +36,4 @@ window.addEventListener('load', function () {
         var newhref = (element.href).replace("https://mybinder.org/v2/gh/data-8/textbook/gh-pages?filepath=", "https://datahub-dev.ung.co/hub/user-redirect/git-sync?repo=https://github.com/lhundt-ung/lhundt-ung.github.io&subPath=");
         element.href = newhref;
     });
-  })
+  })*/
