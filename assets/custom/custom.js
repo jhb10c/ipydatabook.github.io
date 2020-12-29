c0902edaf822fba67e5e06ccd2d0d9cd39497273
ipydatabook <jhbergschneider@ung.edu>
2020-12-29 15:58:56 -0500

    Update custom.js
    Update from Lance's Copy to class copy

--- assets/custom/custom.js
+++ assets/custom/custom.js
@@ -3,8 +3,8 @@
 window.onload = function() {
     var anchors = document.querySelectorAll('a[href*="mybinder.org"]');
     Array.prototype.forEach.call(anchors, function (element, index) {
-        element.href.replace("https://mybinder.org/v2/gh/data-8/textbook/gh-pages?filepath=", "https://datahub.ung.edu/hub/user-redirect/git-sync?repo=https://github.com/lhundt-ung/lhundt-ung.github.io&subPath=");
-        var newhref = (element.href).replace("https://mybinder.org/v2/gh/data-8/textbook/gh-pages?filepath=", "https://datahub.ung.edu/hub/user-redirect/git-sync?repo=https://github.com/lhundt-ung/lhundt-ung.github.io&subPath=");
+        element.href.replace("https://mybinder.org/v2/gh/data-8/textbook/gh-pages?filepath=", "https://datahub.ung.edu/hub/user-redirect/git-sync?repo=https://github.com/ipydatabook/ipydatabook.github.io&subPath=");
+        var newhref = (element.href).replace("https://mybinder.org/v2/gh/data-8/textbook/gh-pages?filepath=", "https://datahub.ung.edu/hub/user-redirect/git-sync?repo=https://github.com/ipydatabook/ipydatabook.github.io&subPath=");
         element.href = newhref;
  
    });
@@ -13,8 +13,8 @@ window.onload = function() {
 window.addEventListener('load', function () {
     var anchors = document.querySelectorAll('a[href*="mybinder.org"]');
     Array.prototype.forEach.call(anchors, function (element, index) {
-        element.href.replace("https://mybinder.org/v2/gh/data-8/textbook/gh-pages?filepath=", "https://datahub.ung.edu/hub/user-redirect/git-sync?repo=https://github.com/lhundt-ung/lhundt-ung.github.io&subPath=");
-        var newhref = (element.href).replace("https://mybinder.org/v2/gh/data-8/textbook/gh-pages?filepath=", "https://datahub.ung.edu/hub/user-redirect/git-sync?repo=https://github.com/lhundt-ung/lhundt-ung.github.io&subPath=");
+        element.href.replace("https://mybinder.org/v2/gh/data-8/textbook/gh-pages?filepath=", "https://datahub.ung.edu/hub/user-redirect/git-sync?repo=https://github.com/ipydatabook/ipydatabook.github.io&subPath=");
+        var newhref = (element.href).replace("https://mybinder.org/v2/gh/data-8/textbook/gh-pages?filepath=", "https://datahub.ung.edu/hub/user-redirect/git-sync?repo=https://github.com/ipydatabook/ipydatabook.github.io&subPath=");
         element.href = newhref;
     });
   })
@@ -22,8 +22,8 @@ window.addEventListener('load', function () {
   window.addEventListener('click', function () {
     var anchors = document.querySelectorAll('a[href*="mybinder.org"]');
     Array.prototype.forEach.call(anchors, function (element, index) {
-        element.href.replace("https://mybinder.org/v2/gh/data-8/textbook/gh-pages?filepath=", "https://datahub.ung.edu/hub/user-redirect/git-sync?repo=https://github.com/lhundt-ung/lhundt-ung.github.io&subPath=");
-        var newhref = (element.href).replace("https://mybinder.org/v2/gh/data-8/textbook/gh-pages?filepath=", "https://datahub.ung.edu/hub/user-redirect/git-sync?repo=https://github.com/lhundt-ung/lhundt-ung.github.io&subPath=");
+        element.href.replace("https://mybinder.org/v2/gh/data-8/textbook/gh-pages?filepath=", "https://datahub.ung.edu/hub/user-redirect/git-sync?repo=https://github.com/ipydatabook/ipydatabook.github.io&subPath=");
+        var newhref = (element.href).replace("https://mybinder.org/v2/gh/data-8/textbook/gh-pages?filepath=", "https://datahub.ung.edu/hub/user-redirect/git-sync?repo=https://github.com/ipydatabook/ipydatabook.github.io&subPath=");
         element.href = newhref;
     });
   })
@@ -31,8 +31,8 @@ window.addEventListener('load', function () {
   window.addEventListener('loadend', function () {
     var anchors = document.querySelectorAll('a[href*="mybinder.org"]');
     Array.prototype.forEach.call(anchors, function (element, index) {
-        element.href.replace("https://mybinder.org/v2/gh/data-8/textbook/gh-pages?filepath=", "https://datahub.ung.edu/hub/user-redirect/git-sync?repo=https://github.com/lhundt-ung/lhundt-ung.github.io&subPath=");
-        var newhref = (element.href).replace("https://mybinder.org/v2/gh/data-8/textbook/gh-pages?filepath=", "https://datahub.ung.edu/hub/user-redirect/git-sync?repo=https://github.com/lhundt-ung/lhundt-ung.github.io&subPath=");
+        element.href.replace("https://mybinder.org/v2/gh/data-8/textbook/gh-pages?filepath=", "https://datahub.ung.edu/hub/user-redirect/git-sync?repo=https://github.com/ipydatabook/ipydatabook.github.io&subPath=");
+        var newhref = (element.href).replace("https://mybinder.org/v2/gh/data-8/textbook/gh-pages?filepath=", "https://datahub.ung.edu/hub/user-redirect/git-sync?repo=https://github.com/ipydatabook/ipydatabook.github.io&subPath=");
         element.href = newhref;
     });
   })
